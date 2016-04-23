a8129d7dedc585edc648ae43b13d7995c7e4dcee
Ivan Bober <ivibober@gmail.com>
2016-04-23 17:03:19 -0300

    Conectar el Swap con la UMC mediante sockets

--- SWAP/src/swap.c
+++ SWAP/src/swap.c
@@ -7,8 +7,28 @@
 
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include <netinet/in.h>
+#include<netinet/ip.h>
+#include<unistd.h>
 
 int main(int argc,char **argv) {
 
+	struct sockaddr_in umcAddress;
+		umcAddress.sin_family = AF_INET;
+		umcAddress.sin_addr.s_addr = inet_addr(argv[1]);
+		umcAddress.sin_port = htons(atoi(argv[2]));
+
+
+		int umc = socket(AF_INET,SOCK_STREAM,0);
+			if(connect(umc,(void*)&umcAddress,sizeof(umcAddress)) != 0) {
+				perror("No se pudo conectar al nucleo.");
+				return EXIT_FAILURE;
+			}
+		close(umc);
+		return EXIT_SUCCESS;
 }
 
