e42d44fa38595b056fd09d1b3e71061c093af868
Franco Albornoz <francoalbornoz@openmailbox.org>
2016-04-25 15:37:54 -0300

    Agrego select() al cpu para poder retrasmitir el mensaje del nucleo a la umc

--- CPU/src/cpu.c
+++ CPU/src/cpu.c
@@ -28,6 +28,16 @@ int main(int argc,char **argv) {
 		return EXIT_FAILURE;
 	}
 
+	int maxfd = 3;				// Indice de maximo FD
+	char buffer[100];			// Bufer para send/recv
+
+	struct timeval tv;			// Estructura para select()
+	tv.tv_sec = 2;
+	tv.tv_usec = 500000;
+
+	fd_set readfds,masterfds;	// Estructuras para select()
+	FD_ZERO(&readfds);
+
 	struct sockaddr_in nucleoAddress;
 	nucleoAddress.sin_family = AF_INET;
 	nucleoAddress.sin_addr.s_addr = inet_addr(argv[1]);
@@ -39,19 +49,33 @@ int main(int argc,char **argv) {
 	umcAddress.sin_port = htons(atoi(argv[4]));
 
 	int nucleo = socket(AF_INET,SOCK_STREAM,0);
+	if (nucleo>maxfd)
+		maxfd = nucleo;			// Se actualiza el indice de maximo fd
 	if(connect(nucleo,(void*)&nucleoAddress,sizeof(nucleoAddress)) != 0) {
 		perror("No se pudo conectar al nucleo.");
 		return EXIT_FAILURE;
 	}
+	FD_SET(nucleo,&masterfds);	// Se agrega socket a la lista de fds
 
 	int umc = socket(AF_INET,SOCK_STREAM,0);
+	if (umc>maxfd)
+		maxfd = umc;			// Se actualiza el indice de maximo fd
 	if(connect(umc,(void*)&umcAddress,sizeof(umcAddress)) != 0) {
 		perror("No se pudo conectar a la umc.");
 		return EXIT_FAILURE;
 	}
+	FD_SET(umc,&masterfds);		// Se agrega socket a la lista de fds
 
 	while(1) {
-		//Falta implementar que sucede cuando se conecta al nucleo y a la umc
+		readfds = masterfds;	// Copio el struct con fds al auxiliar para read
+		select(maxfd+1,&readfds,NULL,NULL,&tv);
+		if (FD_ISSET(nucleo, &readfds))		// Si el nucleo envio algo
+		{
+			recv(nucleo,buffer,100,0);
+			printf("El nucleo informa lo siguiente: %s\nMensaje enviado a la UMC.\n",buffer);
+			send(umc,buffer,100,0);
+
+		}
 	}
 
 	close(nucleo);
