1560909ebe0ae393a3fa7e331a6cfa8e565dad3f
MicaelaParedes <micaelabparedes@gmail.com>
2016-04-30 00:07:33 -0300

    Modifico el 256 de algunas lineas por sizeof(buf)

--- UMC/src/umc.c
+++ UMC/src/umc.c
@@ -22,9 +22,9 @@
 int main(int argc,char **argv)
 {
 	if (argc != 3) {
-			perror("No se ingreso la cantidad de parametros correspondientes");
-			return EXIT_FAILURE;
-		}
+		perror("No se ingreso la cantidad de parametros correspondientes");
+		return EXIT_FAILURE;
+	}
 
 	fd_set master; 	// Conjunto maestro de descriptores de fichero
 	fd_set read_fds; // Conjunto temporal de descriptores de fichero
@@ -91,7 +91,7 @@ int main(int argc,char **argv)
 		exit(1);
 	}
 
-//	memset(&(direccionServidor.sin_zero), '\0', 8);
+	//	memset(&(direccionServidor.sin_zero), '\0', 8);
 	if (bind(listenernucleo, (struct sockaddr*)&direccionNucleo, sizeof(direccionNucleo)) == -1) {
 		perror("bind");
 
@@ -129,7 +129,7 @@ int main(int argc,char **argv)
 	}
 
 	// Bucle principal, todavía no es necesario
-/*	for(;;) {
+	/*	for(;;) {
 		read_fds = master;
 		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
 			perror("select");
@@ -137,80 +137,80 @@ int main(int argc,char **argv)
 		}  */
 
 	swapfd = socket(AF_INET,SOCK_STREAM,0);
-		if (swapfd>fdmax)
-			fdmax = swapfd;			// Actualizar el maximo
-		if(connect(swapfd,(void*)&direccionSwap,sizeof(direccionSwap)) != 0) {
-			perror("No se pudo conectar al swap.");
-			return EXIT_FAILURE;
-		}
+	if (swapfd>fdmax)
+		fdmax = swapfd;			// Actualizar el maximo
+	if(connect(swapfd,(void*)&direccionSwap,sizeof(direccionSwap)) != 0) {
+		perror("No se pudo conectar al swap.");
+		return EXIT_FAILURE;
+	}
 
-		FD_SET(swapfd,&master);		// Añadir swap al conjunto maestro
+	FD_SET(swapfd,&master);		// Añadir swap al conjunto maestro
 
 
 	// Gestionar las conexiones
-				// Gestionar conexion del nucleo
+	// Gestionar conexion del nucleo
 
-					if ((nucleofd = accept(listenernucleo, (struct sockaddr*)&direccionNucleo, &addrlen)) == -1)
-					{
-						perror("accept");
-					} else {
-						FD_SET(nucleofd, &master);  // Añadir nucleo al conjunto maestro
-						if (nucleofd > fdmax) {    	// Actualizar el máximo
-							fdmax = nucleofd;
-						}
-						printf("UMC: Nueva conexion desde %s en "
-								"socket %d\n", inet_ntoa(direccionNucleo.sin_addr),nucleofd);
-					}
-
-
-					// Gestionar datos del nucleo
-
-					if ((mensajeNucleo = recv(nucleofd, buf, sizeof(buf), 0)) <= 0) {
-						// Error o conexión cerrada por el cliente
-						if (mensajeNucleo == 0) {
-							// Conexión cerrada
-							printf("UMC: Select: El nucleo %d se ha desconectado\n", nucleofd);
-						} else {
-							perror("recv");
-						}
-
-					printf("UMC: El mensaje:  %s\n del nucleo ha sido enviado al Swap.\n",buf);
-					send(swapfd,buf,sizeof(buf),0);
-
-						FD_CLR(nucleofd, &master); // Eliminar del conjunto maestro
-						bzero(&buf,sizeof(buf));            // Vaciar buffer
-
-				// Gestionar conexion del cpu
-
-						if ((cpufd = accept(listenercpu, (struct sockaddr*)&direccionCPU, &addrlen)) == -1)
-											{
-												perror("accept");
-											} else {
-												FD_SET(cpufd, &master);  // Añadir nucleo al conjunto maestro
-												if (cpufd > fdmax) {    	// Actualizar el máximo
-													fdmax = cpufd;
-												}
-												printf("UMC: Nueva conexion desde %s en "
-														"socket %d\n", inet_ntoa(direccionCPU.sin_addr),cpufd);
-											}
+	if ((nucleofd = accept(listenernucleo, (struct sockaddr*)&direccionNucleo, &addrlen)) == -1)
+	{
+		perror("accept");
+	} else {
+		FD_SET(nucleofd, &master);  // Añadir nucleo al conjunto maestro
+		if (nucleofd > fdmax) {    	// Actualizar el máximo
+			fdmax = nucleofd;
+		}
+		printf("UMC: Nueva conexion desde %s en "
+				"socket %d\n", inet_ntoa(direccionNucleo.sin_addr),nucleofd);
+	}
 
 
-											// Gestionar datos del nucleo
+	// Gestionar datos del nucleo
 
-											if ((mensajeCPU = recv(cpufd, buf, sizeof(buf), 0)) <= 0) {
-												// Error o conexión cerrada por el cliente
-												if (mensajeCPU == 0) {
-													// Conexión cerrada
-													printf("UMC: Select: La CPU %d se ha desconectado\n", cpufd);
-												} else {
-													perror("recv");
-												}
+	if ((mensajeNucleo = recv(nucleofd, buf, sizeof(buf), 0)) <= 0) {
+		// Error o conexión cerrada por el cliente
+		if (mensajeNucleo == 0) {
+			// Conexión cerrada
+			printf("UMC: Select: El nucleo %d se ha desconectado\n", nucleofd);
+		} else {
+			perror("recv");
+		}
 
-											printf("UMC: El mensaje:  %s\n de la cpu ha sido enviado al Swap.\n",buf);
-											send(swapfd,buf,256,0);
+		printf("UMC: El mensaje:  %s\n del nucleo ha sido enviado al Swap.\n",buf);
+		send(swapfd,buf,sizeof(buf),0);
 
-												FD_CLR(nucleofd, &master);  // Eliminar del conjunto maestro
-												bzero(&buf,256);             // Vaciar buffer
+		FD_CLR(nucleofd, &master); // Eliminar del conjunto maestro
+		bzero(&buf,sizeof(buf));            // Vaciar buffer
+
+		// Gestionar conexion del cpu
+
+		if ((cpufd = accept(listenercpu, (struct sockaddr*)&direccionCPU, &addrlen)) == -1)
+		{
+			perror("accept");
+		} else {
+			FD_SET(cpufd, &master);  // Añadir nucleo al conjunto maestro
+			if (cpufd > fdmax) {    	// Actualizar el máximo
+				fdmax = cpufd;
+			}
+			printf("UMC: Nueva conexion desde %s en "
+					"socket %d\n", inet_ntoa(direccionCPU.sin_addr),cpufd);
+		}
+
+
+		// Gestionar datos del nucleo
+
+		if ((mensajeCPU = recv(cpufd, buf, sizeof(buf), 0)) <= 0) {
+			// Error o conexión cerrada por el cliente
+			if (mensajeCPU == 0) {
+				// Conexión cerrada
+				printf("UMC: Select: La CPU %d se ha desconectado\n", cpufd);
+			} else {
+				perror("recv");
+			}
+
+			printf("UMC: El mensaje:  %s\n de la cpu ha sido enviado al Swap.\n",buf);
+			send(swapfd,buf,sizeof(buf),0);
+
+			FD_CLR(nucleofd, &master);  // Eliminar del conjunto maestro
+			bzero(&buf,sizeof(buf));             // Vaciar buffer
 
 
 		}
