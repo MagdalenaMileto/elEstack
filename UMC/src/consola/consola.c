dd8220fca0f9b79498ba19077b3930242e527aee
MagdalenaMileto <maggiemileto@gmail.com>
2016-06-16 18:16:00 -0300

    Prosigo con las entradas que se hacen a traves de la consola. Ya esta hecho el flush, tanto de la TLB como de la memoria. Y la actualizacion del retardo.

--- UMC/src/consola/consola.c
+++ UMC/src/consola/consola.c
@@ -14,20 +14,46 @@ void esperar_comando(void * parametros) {
 
 	getline(&comando_a_realizar, &tamanio_maximo, stdin);
 
+	comando_a_realizar = string_drop_from_end(comando_a_realizar, 1);
+
+	pthread_mutex_lock(&semaforo_mutex_cpu);
 	char ** palabras = string_split(comando_a_realizar, " ");
 	char * primer_palabra = palabras[0];
-	char * segunda_palabra = string_drop_from_end(palabras[1], 1);
 
 	if (string_equals_ignore_case(primer_palabra, "retardo")) {
 
-		int retardo_numerico = atoi(segunda_palabra);
-		cambiar_retardo(retardo_numerico);
+		char * segunda_palabra = palabras[1];
+
+		if (isNumber(segunda_palabra)) {
+
+			int retardo_numerico = atoi(segunda_palabra);
+			cambiar_retardo(retardo_numerico);
+
+		} else {
+
+			goto error;
+		}
 
 	} else if (string_equals_ignore_case(primer_palabra, "dump")) {
 
-		//Dump.
+		//TODO: Dump all.
+
+		char * segunda_palabra = palabras[1];
+
+		if (isNumber(segunda_palabra)) {
+
+			int proceso_a_dumpear = atoi(segunda_palabra);
+
+			//TODO: dumpear este proceso
+
+		} else {
+
+			goto error;
+		}
 
 	} else if (string_equals_ignore_case(primer_palabra, "flush")) {
+
+		char * segunda_palabra = palabras[1];
 
 		if (string_equals_ignore_case(segunda_palabra, "tlb")) {
 
@@ -35,17 +61,28 @@ void esperar_comando(void * parametros) {
 
 		} else if (string_equals_ignore_case(segunda_palabra, "memory")) {
 
-			//Memory.
+			char * tercer_palabra = palabras[2];
+
+			if (isNumber(tercer_palabra)) {
+
+				int pid = atoi(tercer_palabra);
+				marcar_las_paginas_del_proceso_como_modificadas(pid);
+
+			} else {
+				goto error;
+			}
 
 		} else {
-
-			printf("El comando a realizar no existe.");
+			goto error;
 		}
 
 	} else {
 
 		//Comando inválido.
+		error: printf("El comando \"%s\" a realizar no existe.",
+				comando_a_realizar);
 	}
+	pthread_mutex_unlock(&semaforo_mutex_cpu);
 }
 
 void cambiar_retardo(int retardo_numerico) {
@@ -53,15 +90,65 @@ void cambiar_retardo(int retardo_numerico) {
 	retardo = retardo_numerico;
 }
 
-void vaciar_tlb(){
+void vaciar_tlb() {
 
-	pthread_mutex_lock(&semaforo_mutex_consola);
 	list_clean(tlb);
 }
 
+void marcar_las_paginas_del_proceso_como_modificadas(int proceso) {
+
+	bool filtrar_por_proceso(void * entrada) {
+
+		t_entrada_tabla_de_paginas * entrada_tabla_paginas =
+				(t_entrada_tabla_de_paginas *) entrada;
+
+		return entrada_tabla_paginas->pid == proceso;
+
+	}
+
+	t_list * lista_filtrada_por_proceso = list_filter(tabla_de_paginas,
+			filtrar_por_proceso);
+
+	void cambiar_bit(void * entrada) {
+
+		t_entrada_tabla_de_paginas * entrada_tabla_paginas =
+				(t_entrada_tabla_de_paginas *) entrada;
+
+		entrada_tabla_paginas->modificado = true;
+
+	}
+
+	list_iterate(lista_filtrada_por_proceso, cambiar_bit);
+
+}
 
 //Funciones Auxiliares:
 char * string_drop_from_end(char * string, int cuantos) {
 
 	return string_reverse(string_reverse(string) + cuantos);
 }
+
+bool isNumber(char * palabra) {
+
+	int tamanio = string_length(palabra);
+
+	t_list * lista_chars = list_create();
+
+	int i;
+	for (i = 0; i < tamanio; i++) {
+
+		list_add(lista_chars, (void *) palabra[i]);
+
+	}
+
+	bool es_digito(void * elemento) {
+
+		char * letra = (char *) elemento;
+
+		return isdigit(letra);
+	}
+
+	return list_all_satisfy(lista_chars, es_digito);
+
+}
+
--- UMC/src/consola/consola.h
+++ UMC/src/consola/consola.h
@@ -10,15 +10,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include <commons/string.h>
 #include <../commons/collections/list.h>
 
 #include "../../src/globales.h"
+#include "../../src/tabla_de_paginas/estructuras.h"
 
 void esperar_comando(void * parametros);
 void cambiar_retardo(int retardo_numerico);
+void * generar_estado_actual();
 void vaciar_tlb();
+void marcar_las_paginas_del_proceso_como_modificadas(int proceso);
 char * string_drop_from_end(char * string, int cuantos);
+
+
+bool isNumber(char * palabra);
 
 #endif /* CONSOLA_CONSOLA_H_ */
