752b87cd277e7df048f6f17323ed02fb6cef336b
MagdalenaMileto <maggiemileto@gmail.com>
2016-06-30 20:48:46 -0300

    UMC arregladita.

--- UMC/src/hilos/cpu.c
+++ UMC/src/hilos/cpu.c
@@ -63,7 +63,6 @@ void atender_cpu(void * parametro_hilo) {
 			log_info(log, "Entro a cambiar proceso");
 
 			memcpy(&pid_actual, paquete_nuevo->data, sizeof(int));
-			proceso_actual = pid_actual;
 
 			log_info(log, "Se cambio el proceso actual a %d.", proceso_actual);
 
